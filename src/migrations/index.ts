77cbfa97e1c1a921cb0b39af22ec23552be7419e
azzaky9 <zakiiws22@gmail.com>
2026-03-07 17:03:12 +0700

    feat(migrations): add api_key columns to users table
    - Added columns `enable_a_p_i_key`, `api_key`, and `api_key_index` to the `users` table.
    - Created an index `users_api_key_idx` on the `api_key_index` column.
    - Implemented migration rollback to drop the added columns and index if necessary.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,10 @@
-import * as migration_20260305_164431_base_schema from './20260305_164431_base_schema'
-import * as migration_20260305_164451_add_network_details from './20260305_164451_add_network_details'
-import * as migration_20260306_154836 from './20260306_154836'
-import * as migration_20260306_171733_add_exchange_fee_fields from './20260306_171733_add_exchange_fee_fields'
-import * as migration_20260306_175925_batch_monitoring from './20260306_175925_batch_monitoring'
-import * as migration_20260307_120000_add_usdt_decimals from './20260307_120000_add_usdt_decimals'
+import * as migration_20260305_164431_base_schema from './20260305_164431_base_schema';
+import * as migration_20260305_164451_add_network_details from './20260305_164451_add_network_details';
+import * as migration_20260306_154836 from './20260306_154836';
+import * as migration_20260306_171733_add_exchange_fee_fields from './20260306_171733_add_exchange_fee_fields';
+import * as migration_20260306_175925_batch_monitoring from './20260306_175925_batch_monitoring';
+import * as migration_20260307_100049_add_api_key_to_users from './20260307_100049_add_api_key_to_users';
+import * as migration_20260307_120000_add_usdt_decimals from './20260307_120000_add_usdt_decimals';
 
 export const migrations = [
   {
@@ -32,8 +33,13 @@ export const migrations = [
     name: '20260306_175925_batch_monitoring',
   },
   {
+    up: migration_20260307_100049_add_api_key_to_users.up,
+    down: migration_20260307_100049_add_api_key_to_users.down,
+    name: '20260307_100049_add_api_key_to_users',
+  },
+  {
     up: migration_20260307_120000_add_usdt_decimals.up,
     down: migration_20260307_120000_add_usdt_decimals.down,
-    name: '20260307_120000_add_usdt_decimals',
+    name: '20260307_120000_add_usdt_decimals'
   },
-]
+];
